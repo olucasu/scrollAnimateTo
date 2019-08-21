04d3ff74a950c38de57e040c9282f0768c18cc0d
Lucas Guilherme Uchôa <lglucasu@gmail.com>
2019-08-21 10:38:28 -0400

    All aboard!

---
+++ scrollAnimateTo.js
@@ -0,0 +1,21 @@
+
+$ = jQuery;
+/**
+ * Scroll Animate To!
+ * ======
+ * Perform a scroll to a element
+ * @param {*} elementOffsetTop
+ * @param {*} speed - Optional
+ */
+const scrollAnimateTo = (elementOffsetTop, speed) => {
+  const page = $('html, body');
+  // Animates to a element
+  page.animate({
+    scrollTop: elementOffsetTop,
+  }, speed !== undefined ? speed : 700);
+  // Stop the animation when the events below happen 
+  page.on('scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove', () => {
+    page.stop();
+  });
+};
+export default scrollAnimateTo;
